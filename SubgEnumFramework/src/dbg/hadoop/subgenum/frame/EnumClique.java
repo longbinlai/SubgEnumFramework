a909d8a55fd2258001efe687325ddb87b86a8d4f
BinLoveMiao <longbin.lai@gmail.com>
2015-08-25 23:22:34 +1000

    Implement frame clique functions

---
+++ SubgEnumFramework/src/dbg/hadoop/subgenum/frame/EnumClique.java
@@ -0,0 +1,280 @@
+package dbg.hadoop.subgenum.frame;
+
+import gnu.trove.map.hash.TLongLongHashMap;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.SequenceFile.CompressionType;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.Logger;
+
+import com.hadoop.compression.lzo.LzoCodec;
+
+import dbg.hadoop.subgraphs.io.HVArray;
+import dbg.hadoop.subgraphs.utils.Config;
+import dbg.hadoop.subgraphs.utils.Graph;
+import dbg.hadoop.subgraphs.utils.InputInfo;
+import dbg.hadoop.subgraphs.utils.CliqueEncoder;
+import dbg.hadoop.subgraphs.utils.Utility;
+
+public class EnumClique {
+	private static InputInfo inputInfo  = new InputInfo();
+	public static String workdir="";
+	public static String filename="";
+
+	public static void main(String[] args) throws Exception {
+		inputInfo = new InputInfo(args);
+		boolean isCountOnly = inputInfo.isCountOnly;
+		String workDir = inputInfo.workDir;
+		
+		if (workDir.toLowerCase().contains("hdfs")) {
+			int pos = workDir.substring("hdfs://".length()).indexOf("/")
+					+ "hdfs://".length();
+			Utility.setDefaultFS(workDir.substring(0, pos));
+		} else {
+			Utility.setDefaultFS("");
+		}
+		
+		Configuration conf = new Configuration();
+		conf.setBoolean("count.only", isCountOnly);
+		conf.setStrings("clique.number.vertices", inputInfo.cliqueNumVertices);
+		if(!isCountOnly){
+			DistributedCache.addCacheFile(new URI(new Path(workDir).toUri()
+					.toString() + "/" + Config.cliques), conf);
+		}
+		long startTime=System.currentTimeMillis();   
+		String[] opts = { workDir + "triangle.res", workDir + "frame.clique.res",	
+					inputInfo.numReducers, inputInfo.jarFile, inputInfo.cliqueNumVertices};
+		
+		ToolRunner.run(conf, new EnumCliqueDriver(), opts);
+		System.out.println("End of Enumeration");
+		
+		long endTime = System.currentTimeMillis();
+		System.out.println(" " + (endTime - startTime) / 1000 + "s");
+		
+		if (inputInfo.isCountCliqueOnce) {
+			String[] opts2 = { workDir + "frame.clique.res", workDir + "frame.clique.cnt", 
+					inputInfo.numReducers, inputInfo.jarFile, inputInfo.cliqueNumVertices };
+			ToolRunner.run(conf, new CliqueCountDriver(), opts2);
+		}
+		
+		//if(isCountOnly){
+			Utility.getFS().delete(new Path(workDir + "frame.clique.cnt"));
+			Utility.getFS().delete(new Path(workDir + "frame.clique.res"));
+		//}
+		//else {
+		//	Utility.getFS().delete(new Path(workDir + "frame.clique.cnt"));
+		//}
+	}
+}
+
+class EnumCliqueDriver extends Configured implements Tool{
+	public int run(String[] args) throws IOException, ClassNotFoundException, InterruptedException, URISyntaxException {
+		Configuration conf = getConf();
+		// The parameters: <triangleDir> <outputDir> <numReducers> <jarFile> <cliqueNumVertices>
+		int numReducers = Integer.parseInt(args[2]);
+		conf.setBoolean("mapred.compress.map.output", true);
+		conf.set("mapred.map.output.compression.codec", "com.hadoop.compression.lzo.LzoCodec");
+		Job job = new Job(conf, "Frame " + args[4] + "-Clique");
+		((JobConf)job.getConfiguration()).setJar(args[3]);
+		job.setMapperClass(EnumCliqueMapper.class);
+		
+		job.setInputFormatClass(SequenceFileInputFormat.class);
+		//job.setOutputFormatClass(TextOutputFormat.class);
+		job.setOutputFormatClass(SequenceFileOutputFormat.class);
+		SequenceFileOutputFormat.setOutputCompressionType(job, CompressionType.BLOCK);
+		SequenceFileOutputFormat.setOutputCompressorClass(job, LzoCodec.class);
+		
+		job.setMapOutputKeyClass(LongWritable.class);
+		job.setMapOutputValueClass(HVArray.class);
+		job.setOutputKeyClass(LongWritable.class);
+		if(conf.getBoolean("count.only", true)){
+			job.setOutputValueClass(LongWritable.class);
+			job.setReducerClass(EnumCliqueCountReducer.class);
+		}
+		else{
+			job.setOutputValueClass(HVArray.class);
+			job.setReducerClass(EnumCliqueEnumReducer.class);
+		}
+		
+		job.setNumReduceTasks(numReducers);
+		
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		job.waitForCompletion(true);
+		return 0;
+	}
+}
+
+class EnumCliqueMapper extends
+	Mapper<NullWritable, HVArray, LongWritable, HVArray> {
+	@Override
+	public void map(NullWritable _key, HVArray _value, Context context)
+			throws IOException, InterruptedException {
+		context.write(new LongWritable(_value.getFirst()),
+				new HVArray(_value.getSecond(), _value.getLast()));
+	}
+}
+
+class EnumCliqueCountReducer extends 
+	Reducer<LongWritable,HVArray, LongWritable, LongWritable> {
+
+	@Override
+	public void reduce(LongWritable _key, Iterable<HVArray> values, Context context) throws IOException, InterruptedException{
+		Graph G = new Graph();
+		int cnt=0;
+		for (HVArray val : values) {
+			cnt = cnt + 1;
+			for (int i = 0; i < val.size(); i = i + 2) {
+				G.addEdge(val.get(i), val.get(i + 1));
+			}
+		}
+		Configuration conf = context.getConfiguration();
+		int k = Integer.parseInt(conf.get("clique.number.vertices"));
+		long numclique = G.countCliquesOfSize(k - 1);
+		context.write(_key,new LongWritable(numclique));
+	}
+}
+
+class EnumCliqueEnumReducer extends
+		Reducer<LongWritable, HVArray, LongWritable, HVArray> {
+	private static TLongLongHashMap cliqueMap = null;
+	@Override
+	public void reduce(LongWritable _key, Iterable<HVArray> values,
+			Context context) throws IOException, InterruptedException {
+		Graph G = new Graph();
+		int cnt = 0;
+		for (HVArray val : values) {
+			cnt = cnt + 1;
+			for (int i = 0; i < val.size(); i = i + 2) {
+				G.addEdge(val.get(i), val.get(i + 1));
+			}
+		}
+		Configuration conf = context.getConfiguration();
+		int k = Integer.parseInt(conf.get("clique.number.vertices"));
+		long[] cliqueEnc = G.enumCliqueOfSize(k - 1, _key.get(), cliqueMap);
+		context.write(_key, new HVArray(cliqueEnc));
+	}
+	
+	@Override
+	public void setup(Context context) throws IOException{
+		Configuration conf = context.getConfiguration();
+		FileSystem fs = FileSystem.get(conf);
+		Path[] paths = DistributedCache.getLocalCacheFiles(conf);
+		for (int i = 0; i < paths.length; ++i) {
+			if (paths[i].toString().contains("part-r-")) {
+				SequenceFile.Reader reader = new SequenceFile.Reader(fs, paths[i], conf);
+				LongWritable key = null;
+				HVArray val = null;
+				try {
+					key = (LongWritable) reader.getKeyClass().newInstance();
+					val = (HVArray) reader.getValueClass().newInstance();
+				} catch (InstantiationException e) {
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				}
+				while(reader.next(key, val)){
+					for(long v : val.toArrays()){
+						cliqueMap.put(v, key.get());
+					}
+				}
+				reader.close();
+				
+			}
+		}
+	}
+}
+
+class CliqueCountDriver extends Configured implements Tool{
+	public int run(String[] args) throws IOException, ClassNotFoundException, InterruptedException, URISyntaxException {
+		Configuration conf = getConf();
+		// The parameters: <cliqueDir> <outputDir> <numReducers> <jarFile> <cliqueNumVertices>
+		int numReducers = Integer.parseInt(args[2]);
+		conf.setBoolean("mapred.compress.map.output", true);
+		conf.set("mapred.map.output.compression.codec", "com.hadoop.compression.lzo.LzoCodec");
+		Job job = new Job(conf, "Frame " + args[4] + "-Clique Count");
+		((JobConf)job.getConfiguration()).setJar(args[3]);
+		
+		job.setInputFormatClass(SequenceFileInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		//SequenceFileOutputFormat.setOutputCompressionType(job, CompressionType.BLOCK);
+		//SequenceFileOutputFormat.setOutputCompressorClass(job, LzoCodec.class);
+		
+		job.setOutputKeyClass(NullWritable.class);
+		job.setOutputValueClass(LongWritable.class);
+		if(conf.getBoolean("count.only", true)){
+			job.setMapperClass(CliqueCountMapper1.class);
+		}
+		else{
+			job.setMapperClass(CliqueCountMapper2.class);
+		}
+		job.setCombinerClass(CliqueCountReducer.class);
+		job.setReducerClass(CliqueCountReducer.class);
+		
+		job.setNumReduceTasks(numReducers);
+		
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		job.waitForCompletion(true);
+		return 0;
+	}
+}
+
+class CliqueCountMapper1 extends
+		Mapper<LongWritable, LongWritable, NullWritable, LongWritable> {
+	@Override
+	public void map(LongWritable _key, LongWritable _value, Context context)
+			throws IOException, InterruptedException {
+		context.write(NullWritable.get(), _value);
+	}
+}
+
+class CliqueCountMapper2 extends
+		Mapper<LongWritable, HVArray, NullWritable, LongWritable> {
+	@Override
+	public void map(LongWritable _key, HVArray _value, Context context)
+			throws IOException, InterruptedException {
+		context.write(NullWritable.get(), new LongWritable(
+				CliqueEncoder.getNumCliquesFromEncodedArray(_value.toArrays())));
+	}
+}
+
+class CliqueCountReducer extends
+	Reducer<NullWritable, LongWritable, NullWritable, LongWritable> {
+	private static Logger log = Logger.getLogger(CliqueCountReducer.class);
+	@Override
+	public void reduce(NullWritable _key, Iterable<LongWritable> values,
+			Context context) throws IOException, InterruptedException {
+		long sum = 0;
+		for(LongWritable val : values){
+			sum += val.get();
+		}
+		System.out.println("# " + context.getConfiguration().get("clique.number.vertices") 
+				+ "-clique: " + sum);
+		context.write(NullWritable.get(), new LongWritable(sum));
+	}
+}
+
---
+++ SubgEnumFramework/src/dbg/hadoop/subgraphs/utils/CliqueEncoder.java
@@ -0,0 +1,83 @@
+package dbg.hadoop.subgraphs.utils;
+
+import gnu.trove.iterator.TLongIterator;
+import gnu.trove.list.array.TLongArrayList;
+import gnu.trove.set.hash.TLongHashSet;
+
+/**
+ * 
+ * The cliques w.r.t. v has v as the minimum vertex and the vertices are all v's neighbors.
+ * Another thing worth knowing is that if there are a large clique K containing v, any cliqueSize - 1
+ * vertices in K with v form a clique. There, we just use the large clique to record all these cliques.
+ * As the result, the Clique Encoding has the form:
+ * (v, cliqueSize - 1, sizeOfVerticesIn{K}, verticesIn{K}, cliqueArray), 
+ * where K is the large clique that contains v
+ */
+public class CliqueEncoder{
+  private int k = 4;
+  private long v = 0;
+  private TLongHashSet cliqueSet;
+  private TLongArrayList normalBuffer;
+  
+  public CliqueEncoder(long curV, int cliqueSize){
+	  k = cliqueSize;
+	  v = curV;
+	  cliqueSet = new TLongHashSet();
+	  normalBuffer = new TLongArrayList();
+  }
+  
+  public void addCliqueVertex(long[] vertices){
+    if(vertices == null){
+      return;
+    }
+    for(int i = 0; i < vertices.length; ++i){
+      if(!cliqueSet.contains(vertices[i])){
+        cliqueSet.add(vertices[i]);
+      }
+    }
+  }
+  
+  public void addNormalVertices(long[] vertices){
+    if(vertices == null){
+      return;
+    }
+    normalBuffer.addAll(vertices);
+  }
+  
+  public long[] getEncodedCliques() {
+    TLongArrayList buffer = new TLongArrayList(cliqueSet.size() + normalBuffer.size() + 3);
+    buffer.add(v);
+    buffer.add((long)k);
+    buffer.add(cliqueSet.size());
+    TLongIterator iter = cliqueSet.iterator();
+    while(iter.hasNext()){
+      buffer.add(iter.next());
+    }
+    buffer.addAll(normalBuffer.toArray());
+    return buffer.toArray();
+  }
+  
+	public static long getNumCliquesFromEncodedArray(long[] array) {
+		long res = 0L;
+		assert (array != null && array.length >= 3);
+		int sizeOfClique = (int) array[1];
+		int sizeOfVerticesInLargeClique = (int) array[2];
+		res += binorm(sizeOfVerticesInLargeClique, sizeOfClique);
+		res += (array.length - 3 - sizeOfVerticesInLargeClique) / sizeOfClique;
+		return res;
+	}
+
+	private static long binorm(int n, int d) {
+		if (n == 0 || n < d) {
+			return 0;
+		}
+		long res = 1;
+		for (int i = 0; i < d; ++i) {
+			res *= (n - i);
+		}
+		for (int i = 1; i <= d; ++i) {
+			res /= i;
+		}
+		return res;
+	}
+}
